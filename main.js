dd059834320bd1c5e495b3d45262c32c1c9f2c18
raguila6 <raguila6@stu.southtexascollege.edu>
2022-08-07 17:16:06 -0500

    refitted to one page all content

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './node_modules/the-new-css-reset/css/reset.css'
 import './style.css'
 
+
 const menuToggle = document.querySelector('.menu-toggle')
 const menu = document.querySelector('.menu')
 
